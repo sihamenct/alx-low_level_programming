9db8bb5674dc5a270c9686ece1a2bb7ad47c9b77
sihamenct <sihamenachat@gmail.com>
2023-07-13 18:03:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+* main - print a line in the standard error
+* Return: 0 (Success)
+*/
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
