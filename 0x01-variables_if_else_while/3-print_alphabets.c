e4d65926cb2334ce1072eaf218f5efbdc064bcff
sihamenct <sihamenachat@gmail.com>
2023-07-14 15:23:32 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - alphABET
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n = 97;
+int m = 65;
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (m <= 90)
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
