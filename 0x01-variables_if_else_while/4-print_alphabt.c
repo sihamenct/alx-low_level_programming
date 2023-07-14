902fc43443de0df785673e4b7a968c756965ff73
sihamenct <sihamenachat@gmail.com>
2023-07-14 15:38:32 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+* main - alphABET
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+if (n == 101 || n == 113)
+{
+n++;
+continue;
+}
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
