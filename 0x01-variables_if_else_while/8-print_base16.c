2e7cadd40d7284aae0499c2e3b1ab1fa6ad59611
sihamenct <sihamenachat@gmail.com>
2023-07-14 16:08:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - Hexadecimal
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n = 48;
+int m = 97;
+while (n <= 57)
+{
+putchar(n);
+n++;
+}
+while (m <= 102)
+{
+putchar(m);
+m++;
+}
+putchar('\n');
+return (0);
+}
