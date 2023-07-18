4a2c95d558072a248135b0d7ef284d67c99e9cf4
sihamenct <sihamenachat@gmail.com>
2023-07-18 15:09:59 -0700

    main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - Function print alphabet 1O times
+* Return: 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 1; j <= 10; j++)
+{
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
