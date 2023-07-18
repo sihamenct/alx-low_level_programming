53eba0c269d4165930a5acae0036ed0caabc8346
sihamenct <sihamenachat@gmail.com>
2023-07-18 16:03:45 -0700

    main.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _abs - absolute value of given number
+* Return: 0 (Success)
+* @n: number to test
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
