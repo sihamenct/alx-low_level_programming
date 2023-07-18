0b4da7549224656e473da99f09abeb1594b7e878
sihamenct <sihamenachat@gmail.com>
2023-07-18 14:47:36 -0700

    main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - combinations of two digits
+* Return: 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int i;
+	for(i = 97; i < 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
