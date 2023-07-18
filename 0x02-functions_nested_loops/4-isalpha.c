bec0f4fabfcd85c0d0ed843e8d173304c518d62b
sihamenct <sihamenachat@gmail.com>
2023-07-18 15:48:17 -0700

    main.h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isalpha - Print letter lowercase and uppercase
+* Return: 0 (Success)
+* @c: character in ASCII code
+*/
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
