8180362a866528e9e4bce73437018c902286ac2b
sihamenct <sihamenachat@gmail.com>
2023-07-18 18:59:26 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - today is the longest day of my life
+* Return: always 0
+*/
+int main(void)
+{
+int n, sum;
+for (n = 0; n < 1024; n++)
+{
+if ((n % 3 == 0) || (n % 5 == 0))
+sum == n;
+}
+printf("%d\n", sum);
+return (0);
+}
