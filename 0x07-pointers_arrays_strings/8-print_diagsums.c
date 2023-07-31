513302aff1d2a7ef7b980661c00feb459c44bfcc
sihamenct <sihamenachat@gmail.com>
2023-07-31 13:52:49 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_diagsums - function that prints the sum of the two diagonals 
+* print_diagsums - function that prints the sum of the two diagonals
 * @a: input
 * @size: input
 * Return: Always 0 (Success)
